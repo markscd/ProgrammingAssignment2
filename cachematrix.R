3a6592176baddd87bf4b4aa683532ce94f487470
markscd <markscd@hotmail.com>
2016-06-05 20:36:03 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,46 +7,49 @@
 # saying that that it is being returned.
 
 makeCacheMatrix <- function(x = matrix()) {
-# Create the matrix to be stored.
-# We can do this by simply creating a 2x2 matrix:
+     # Create the matrix to be stored.
+     # We can do this by simply creating a 2x2 matrix:
      # m <- matrix(1:4,2,2)
-# We can then see the expected results by entering:
+     # We can then see the expected results by entering:
      # solve(m)
-# Then, we can test our function with the matrix we created for 'm' in the variable 'm2':
+     # Then, we can test our function with the matrix we created for 'm' in the variable 'm2':
      # m2 <- makeCacheMatrix(m)
-# Then, by entering our secondary function, we should notice that it's calculating the results,
-# and the results should be the same as the solve(m) function.
+     # Then, by entering our secondary function, we should notice that it's calculating the results,
+     # and the results should be the same as the solve(m) function.
      # cacheSolve(m2)
-# But, when we enter the function again, we should get the message that it's obtaining the
-# cached results, with it skipping the other parameters.
+     # But, when we enter the function again, we should get the message that it's obtaining the
+     # cached results, with it skipping the other parameters.
      # cacheSolve(m2)
-# Thus, the message "Obtaining cached data" should appear each successive time we use 
-# the function ~ cacheSolve(m2)
+     # Thus, the message "Obtaining cached data" should appear each successive time we use 
+     # the function ~ cacheSolve(m2)
      
      # Initially set the inverse "m" to null
      m <- NULL
      
      # Reset the matrix 'x' a new value via Set function; 
-     set <- function(y) {
+     set <- function(y)            {
           x <<- y
           
           # 'm' (inverse) is also reset to Null
-          m <<- NULL}
+          m <<- NULL
+                                   }
      
      # Return the matrix
      get <- function() x
-     
-     # After the inverse is calculated, the vector's inverse attribute value is set
-     setInverse <- function(solve) {m <<- solve}
-     
-     # The inverse value is returned
-     getInverse <- function() m
-     
-     # List the methods inside of makeCacheMatrix
-     list(set = set,
-          get = get,
-          setInverse = setInverse,
-          getInverse = getInverse)}
+               {
+          # After the inverse is calculated, the vector's inverse attribute value is set
+          setInverse <- function(solve) {m <<- solve}
+          
+          # The inverse value is returned
+          getInverse <- function() m
+          
+          # List the methods inside of makeCacheMatrix
+          list(set = set,
+               get = get,
+               setInverse = setInverse,
+               getInverse = getInverse)
+               }
+                                             }
 
 
 cacheSolve <- function(x, ...) {
@@ -66,22 +69,22 @@ cacheSolve <- function(x, ...) {
      
      # Else, if getInverse has not been used to store the data,
      # then 'get' matrix stored in the vector and put in 'data'
-     else {
-     
-     # get: "Return the Value of a Named Object" -- CRAN
-     data <- x$get()
-                    {
-     # Apply the solve function for the returned data.
-     # solve: "This generic function solves the equation
-     # a %*% x = b for x, where b can be either a vector or a matrix." -- CRAN
-     m <- solve(data)
-     
-     # The results are stored in the special vector, which is the inverse
-     x$setInverse(m)
+     else           {
+          
+          # get: "Return the Value of a Named Object" -- CRAN
+          data <- x$get()
+               {
+               # Apply the solve function for the returned data.
+               # solve: "This generic function solves the equation
+               # a %*% x = b for x, where b can be either a vector or a matrix." -- CRAN
+               m <- solve(data)
+               
+               # The results are stored in the special vector, which is the inverse
+               x$setInverse(m)
+               }
                     }
-          }
      
      # Return the inverse of the matrix whether already known (skipped 'else')
      # or if calculated (within 'else' structure).  1 less line of code.
      return(m)
-                              }
+                                   }
